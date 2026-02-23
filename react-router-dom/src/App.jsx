14bdd73b0160a40bc6f75899cf604e11bd02c4f8
sneha <sneha0122345@gmail.com>
2026-02-24 00:30:13 +0530

    learning routing

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+       <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/About' element={<About />}/>
+        <Route path='/Contact' element={<Contact />}/>
+        <Route path='/Product' element={<Product />}/>
+       </Routes>
+         
+       
+    </div>
+  )
+}
+
+export default App
---
+++ react-router-dom/src/pages/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <h1>Product Page</h1>
+  )
+}
+
+export default Product
